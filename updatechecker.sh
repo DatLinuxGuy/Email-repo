203ad31093c498d467298943af79676075cccb52
DatLinuxGuy <52567870+DatLinuxGuy@users.noreply.github.com>
2020-09-05 12:58:33 +0100

    Delete updatechecker.sh

--- updatechecker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-# This will check for updates and then send and email letting the user know if there is updates
-
-if  sudo apt update | grep "packages can be upgraded." ;then
-	echo ""Subject: OS Upgrades Found on echo $USER"'s" node - Please review"" > ~/code/upgradeable_apps.txt
-	apt list --upgradable -v >>  ~/code/upgradeable_apps.txt 2>&1
-	sed -i.bak '3,2d;5d'  ~/code/upgradeable_apps.txt
-	sendmail ethereum2.0@yahoo.com <  ~/code/upgradeable_apps.txt
-fi
